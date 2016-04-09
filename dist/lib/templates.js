7958b4d69bd99a9dc98be68f5addbd788922ffe0
freedomson <philip.almeida@sapo.pt>
2016-04-09 15:37:20 +0100

    Dist ready

--- dist/lib/templates.js
+++ dist/lib/templates.js
@@ -5,5 +5,5 @@ angular.module('templates-dist', []).run(['$templateCache', function($templateCa
   $templateCache.put("src/features/branding/contact.html",
     "<ul class=contact><li class=contact-item ng-repeat=\"contact in contacts\">{{ contact }}</li></ul>");
   $templateCache.put("src/features/todos/todos.html",
-    "<form id=todo-form ng-submit=addCurrentTodo() ng-if=false>Enter new todo here: <input ng-model=newTodo placeholder=\"enter new todo\"></form>d<ul><li class=todo ng-repeat=\"todo in todos\">{{ todo }}</li></ul>");
+    "<form id=todo-form ng-submit=addCurrentTodo() ng-if=false>Enter new todo here: <input ng-model=newTodo placeholder=\"enter new todo\"></form><ul><li class=todo ng-repeat=\"todo in todos\">{{ todo }}</li></ul>");
 }]);
