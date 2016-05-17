8edaf59bdbc000f264833bbe973b2a3cebb81a61
sky-philipalmeida <philip.almeida@sky.uk>
2016-05-18 00:13:27 +0100

    Focus

--- src/features/branding/branding.js
+++ src/features/branding/branding.js
@@ -4,7 +4,7 @@ angular.module('Branding', [/*'templates-dist'*/])
         function($scope, cssInjector, $translate,  $rootScope) {
 
           $scope.vm = {
-            name : 'Gugabooks',
+            name : 'pliik',
             slug: '',
             logo : 'images/logo.png'
           };
